6f9294db05e9281151fdfb166c260ff93ffa6ee3
captaingenius <tkdantony@hotmail.com>
2014-11-04 17:13:01 -0500

    Update gaussSeidel.m

--- gaussSeidel.m
+++ gaussSeidel.m
@@ -4,12 +4,17 @@ function X = gaussSeidel(K, F)
     N = length(K);
     
     %'currentGuess' is the most up to date values for X - change it below
-    %to modify the initial guess
+    %to modify the initial guess.
     currentGuess = zeros(N,1);
     
     %'relativeTolerance' is the tolerance value below which iterations for
-    %the Gauss-Seidel method are halted. 
+    %the Gauss-Seidel method are halted. Change the tolerance below.
     relativeTolerance = 0.01;
+    
+    %'toleranceFlag' is used to signal that iterations should be stopped. 
+    %When the flag is set, the program completes the current iteration and
+    %then exits. So long as one value of x[i] is not below the tolerance at
+    %an iteration, the program will continue.
     toleranceFlag = false;
     
     while(toleranceFlag ~= true)
@@ -18,6 +23,7 @@ function X = gaussSeidel(K, F)
         
         for i = 1:N
             
+            toleranceFlag = true;
             summation = 0;
             
             for j = 1:N
@@ -34,9 +40,9 @@ function X = gaussSeidel(K, F)
             
             error = abs(currentGuess(i)-previousGuess(i))/abs(previousGuess(i));
                     
-            if (error < relativeTolerance)
+            if (error >= relativeTolerance)
                 
-                toleranceFlag = true;
+                toleranceFlag = false;
                 
             end
             
