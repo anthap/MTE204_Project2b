732322fb6ec3d182dda3cec5cda385aba7726220
abrakoff <adam.rakoff@gmail.com>
2014-11-09 18:51:25 -0500

    Revert "Nik Walks adjusted"
    This reverts commit 2eaf40f90ec2437cad215a564cbee6e49e5bcd69.

--- NikWalks.m
+++ NikWalks.m
@@ -1,10 +1,13 @@
+% take in the iteration number and time step and current applied force
+% matrix; outputs the applied forces with Nik walking on it
+
 function FWalk = NikWalks(Fapplied, j, timeStep)
 
     t = timeStep * j; 
     i = floor(t / 1.6);
     % Nik doesn't start walking on the first element and gets off before
     % last element
-    if (i == 0 || i == 512)
+    if (i == 0 || i == 354)
         FWalk = Fapplied;
         
     else
@@ -14,7 +17,7 @@ function FWalk = NikWalks(Fapplied, j, timeStep)
         Fnow = (-863.28/2)*sin(pi/1.6 * t) + (863.28/2); 
 
         FWalk = Fapplied;
-        Fwalk((i*2),1) = Fnow + Fapplied((i*2),1); 
-        Fwalk(((i-1)*2),1) = Fprev + Fapplied(((i-1)*2),1);
+        Fwalk(i+1) = Fnow + Fapplied(i+1); 
+        Fwalk(i) = Fprev + Fapplied(i);
     end
 end
